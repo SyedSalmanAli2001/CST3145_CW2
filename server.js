f3d88678eb04f4bddabf515940a3cc8bc5db8902
Syed Salman <syed.salman.ali@outlook.com>
2023-02-11 11:55:01 +0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,62 @@
+const ObjectID = require('mongodb').ObjectID;
+
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+app.set('port', 3000);
+
+
+const MongoClient = require('mongodb').MongoClient;
+const path = require("path");
+const fs = require("fs");
+
+let db;
+MongoClient.connect('mongodb+srv://SyedSalman:Thedarkknight01@gettingstarted.hgg5baw.mongodb.net', (err, client) => {
+    db = client.db('Coursework_2_Database')
+})
+
+app.use("/",function(request, response, next) {
+    console.log("In comes a " + request.method + " to " + request.url);
+    next();
+    });
+
+app.get('/', (req, res, next) => {
+    res.send('Select a collection')
+})
+
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    })
+})
+
+app.post('/collection/:collectionName', (req, res, next) => {  
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)    
+        res.send(results.ops)
+      })
+    })
+
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.updateOne(
+        {_id: new ObjectID(req.params.id)},
+        {$set: req.body},
+        {safe: true, multi: false},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result) ? {msg: 'success'} : {msg: 'error'})
+})
+})
+
+app.listen(3000, () => {
+    console.log('Express.js server localhost 3000')
+})
